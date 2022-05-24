d70591f58818be57220e6ef803fc3dca1d46ec0c
Марина Клейн <marinaklejn@MacBook-Air-Marina.local>
2022-05-25 01:57:01 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 
-export let userName = readlineSync.question('May I have your name? ');
+export const userName = readlineSync.question('May I have your name? ');
 
-console.log("Hello " + userName + "!");
+console.log(`Hello ${userName}!`);
