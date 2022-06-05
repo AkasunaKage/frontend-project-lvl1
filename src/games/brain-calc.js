239c266371465e0183446e0ec666137f0aa77b96
Марина Клейн <marinaklejn@MacBook-Air-Marina.local>
2022-06-05 20:41:38 +0300

    add games

---
+++ src/games/brain-calc.js
@@ -0,0 +1,33 @@
+import gamesLogic from '../src/index.js';
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+const rule = 'What is the result of the expression?';
+
+const calc = (number, sign, number2) => {
+  let result;
+  if (sign === '+') {
+    result = (number + number2);
+  } else if (sign === '-') {
+    result = (number - number2);
+  } else if (sign === '*') {
+    result = (number * number2);
+  }
+  return result;
+};
+
+const getQuestionAnswer = () => {
+  const number = getRandomInt(100);
+  const number2 = getRandomInt(100);
+
+  const signs = ['+', '-', '*'];
+  const sign = signs[getRandomInt(3)];
+
+  const userQuestion = `${number} ${sign} ${number2}`;
+
+  const correctAnswer = (calc(number, sign, number2)).toString();
+
+  return [userQuestion, correctAnswer];
+};
+
+gamesLogic(rule, getQuestionAnswer);
---
+++ src/games/brain-even.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+import userName from '../src/cli.js';
+
+console.log(`Hello ${userName}!`);
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const evenGame = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandomInt(100);
+    console.log('Question:', number);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    const check1 = (number % 2 === 0) && (userAnswer === 'yes');
+    const check2 = (number % 2 !== 0) && (userAnswer === 'no');
+
+    if (check1 === true || check2 === true) {
+      console.log('Correct!');
+    } else {
+      const opposite = (userAnswer === 'yes' ? 'no' : 'yes');
+      const wrong = console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was '${opposite}'.\nLet's try again, ${userName}!`);
+      return wrong;
+    }
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+evenGame();
