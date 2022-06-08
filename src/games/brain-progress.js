a77d1c1a935c41f91ef21e5c79362a2d34116801
Марина Клейн <marinaklejn@MacBook-Air-Marina.local>
2022-06-08 23:46:44 +0300

    add brain-progress

---
+++ src/games/brain-progress.js
@@ -0,0 +1,28 @@
+import gamesLogic from '../index.js';
+
+import getRandomInt from '../randomFunction.js';
+
+const rule = 'What number is missing in the progression?';
+
+const generateProgression = (number, progresInt, progresLength) => {
+  const progressionArray = [];
+  for (let i = number; progressionArray.length < progresLength; i += progresInt) {
+    progressionArray.push(i);
+  }
+  return progressionArray;
+};
+
+const getQuestionAnswer = () => {
+  const number = getRandomInt(5, 10);
+  const progresInt = getRandomInt(5, 9);
+  const progresLength = getRandomInt(10);
+  const progression = generateProgression(number, progresInt, progresLength);
+  const index = getRandomInt(2, progresLength);
+  const correctAnswer = String(progression[index]);
+  progression[index] = '..';
+  const userQuestion = progression.join(' ');
+
+  return [userQuestion, correctAnswer];
+};
+
+gamesLogic(rule, getQuestionAnswer);
