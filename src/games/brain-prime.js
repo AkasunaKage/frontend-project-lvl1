60a69dbd6943643585d7ff34a7a530680e62ddb4
Марина Клейн <marinaklejn@MacBook-Air-Marina.local>
2022-06-09 00:52:13 +0300

    add brain-prime

---
+++ src/games/brain-prime.js
@@ -0,0 +1,25 @@
+import gamesLogic from '../index.js';
+
+import getRandomInt from '../randomFunction.js';
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getQuestionAnswer = () => {
+  const userQuestion = getRandomInt(2, 100);
+  const correctAnswer = getPrime(userQuestion) ? 'yes' : 'no';
+  return [userQuestion, correctAnswer];
+};
+
+gamesLogic(rule, getQuestionAnswer);
