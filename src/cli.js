c27e7e82f58a31c780ef39ee25663268982dec91
Марина Клейн <marinaklejn@MacBook-Air-Marina.local>
2022-06-11 02:28:35 +0300

    cganged games and cli

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const userName = () => readlineSync.question('May I have your name? ');
+const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+};
 
-export default userName;
+export default getUserName;
