1af9d1fb3035508c7b2aa406959d873b5394f478
Марина Клейн <marinaklejn@MacBook-Air-Marina.local>
2022-06-11 14:38:25 +0300

    fix brain-progress

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { rule, getQuestionAnswer } from '../src/games/brain-progress.js';
+import { rule, getQuestionAnswer } from '../src/games/brain-progression.js';
 import gamesLogic from '../src/index.js';
 
 gamesLogic(rule, getQuestionAnswer);
